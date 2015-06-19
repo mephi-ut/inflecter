59206b9d73c2b3f566dcbbdb302229deea2c770c
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2015-06-19 12:37:31 +0300

    Added support of stdin

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
     пачке абрикосов
     $ ./doinflect.php noun 2 "$(echo пачка абрикосов | base64)"
     пачку абрикосов
+    $ echo 'пачка абрикосов' | ./doinflect.php noun 1
+    пачке абрикосов
+    $ echo 'пачка абрикосов' | ./doinflect.php noun 2
+    пачку абрикосов
--- doinflect.php
+++ doinflect.php
@@ -7,7 +7,11 @@ $inflect = new Inflect();
 
 $wordType = $argv[1];
 $case     = $argv[2];
-$sentence = base64_decode($argv[3]);
+
+$sentence = @base64_decode($argv[3]);
+if (strlen($sentence) <= 0) {
+	$sentence = file_get_contents("php://stdin");
+}
 
 switch ($wordType) {
 	case "noun":
