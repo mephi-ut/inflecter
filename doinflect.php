28d1b08a40942717205c744180f7a72dc28d5b0a
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2016-04-19 09:04:14 +0300

    Updated to new version on library "Inflect"

--- doinflect.php
+++ doinflect.php
@@ -15,9 +15,11 @@ if (strlen($sentence) <= 0) {
 
 switch ($wordType) {
 	case "noun":
+		/*
 		$words    = split(' ', $sentence);
 		$words[0] = $inflect->getInflectNoun($words[0], $case);
-		print join(' ', $words);
+		print join(' ', $words);*/
+		print $inflect->getInflectNoun($sentence, $case);
 		return 0;
 	default:
 		die('unknown word type');
